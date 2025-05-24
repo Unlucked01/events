b7e9d578b4f03f87a4c311f6dfc8d87c0c628e68
Egor Kopylov <113236909+Unlucked01@users.noreply.github.com>
2025-05-24 13:56:08 +0300

    fix

--- frontend_v2/next.config.js
+++ frontend_v2/next.config.js
@@ -3,12 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost', 'example.com', 'images.unsplash.com', 'localhost:8000'], // Домены для изображений
+    domains: ['localhost', 'example.com', 'images.unsplash.com', 'localhost:8000', 'unl-events.duckdns.org'], // Домены для изображений
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '8000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unl-events.duckdns.org',
         pathname: '/**',
       },
     ],
